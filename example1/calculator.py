7066b9c05a6fca412aeb5a69671de8c004772d65
natmurei <47848668+natmurei@users.noreply.github.com>
2019-04-11 13:43:45 +0300

    added new file

---
+++ example1/calculator.py
@@ -0,0 +1,33 @@
+def add(a,b):
+	answer=a+b
+	return answer
+
+def subtract(a,b):
+	answer=a-b
+	return answer
+
+def multiply(a,b):
+    answer=a*b
+    return answer
+
+def modulus(a,b):
+    answer=a%b
+    return answer
+
+def divide(a,b):
+    answer=a/b
+    return answer
+
+def exponent(a,b):
+    answer=a**b
+    return answer
+
+def percentage(a,b):
+    answer=a%b
+    return answer
+
+def whole_number(a,b):
+    answer=a//b
+    return answer
+
+
